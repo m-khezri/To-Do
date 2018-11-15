e2f25ebc678cf21e9b820e0d7bf1a91efd8be0b7
mohammad <khezri@outlook.com>
2018-11-14 23:02:29 -0600

    ui navbar

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import apiKeys from '../db/apiKeys.json';
 import 'bootstrap';
 import './index.scss';
 import logInbutton from './components/auth/auth';
+import createNavbar from './components/navbar/navbar';
 
 const initApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  createNavbar();
   logInbutton();
 };
 
